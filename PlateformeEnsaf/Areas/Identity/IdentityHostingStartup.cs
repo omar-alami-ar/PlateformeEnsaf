c36bd59d91715c223d83d91d7c479f923b01cf0e
Omar <omaralamiaroussi@gmail.com>
2022-02-23 16:55:38 +0100

    Add project files.

---
+++ PlateformeEnsaf/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PlateformeEnsaf.Data;
+
+[assembly: HostingStartup(typeof(PlateformeEnsaf.Areas.Identity.IdentityHostingStartup))]
+namespace PlateformeEnsaf.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
